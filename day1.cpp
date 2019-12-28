b0d53ba81a1786094fc67e3b4e39db193e7e51fa
Francis Duvivier <francisduvivier@gmail.com>
2019-12-28 15:57:08 +0100

    ✨ Solve Day 1 Part 1 and 2

--- day1.cpp
+++ day1.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+int countChars(char searchChar, std::string allLines) {
+    int count = 0;
+    for (char &c : allLines) {
+        if (c == searchChar) {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int findFirstPosForLevel(std::string allLines, int level) {
+    int count = 0;
+    for (char &c : allLines) {
+        if (c == '(') {
+            count++;
+        } else {
+            count--;
+        }
+        if (count == level) {
+            return &c - &allLines[0] + 1;
+        }
+    }
+    return -1;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::string line;
+    std::string allLines = "";
+    int nbLines = 0;
+    std::ifstream myfile("input.txt");
+    if (myfile.is_open()) {
+        while (getline(myfile, line)) {
+            allLines += line;
+            nbLines++;
+        }
+        myfile.close();
+    } else { std::cout << "Unable to open file"; }
+
+    std::cout << "Part 1: endFloor: " << allLines.size() - 2 * countChars(')', allLines) << "\n";
+    std::cout << "Part 2: basePos: " << findFirstPosForLevel(allLines, -1) << "\n";
 }
