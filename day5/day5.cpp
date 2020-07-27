a89a4fc161493e9f6c0ec0ecb70eb4fb8eda1fb5
Francis Duvivier <francisduvivier@gmail.com>
2020-07-27 16:48:43 +0200

    ✨ Solve Day 5 Part 2

--- day5/day5.cpp
+++ day5/day5.cpp
@@ -11,6 +11,7 @@ bool has3Vowels(string input)
     cmatch m;
     return regex_search(input.c_str(), m, re);
 }
+
 bool hasNoBadDuels(string input)
 {
     regex re("ab|cd|pq|xy");
@@ -20,12 +21,25 @@ bool hasNoBadDuels(string input)
 
 bool hasDoubleLetter(string input)
 {
-
     regex re("(.)\\1");
     cmatch m;
     return regex_search(input.c_str(), m, re);
 }
-bool isNiceString(string input)
+
+bool hasPairDouble(string input)
+{
+    regex re("(..).*\\1");
+    cmatch m;
+    return regex_search(input.c_str(), m, re);
+}
+bool hasBrokenDouble(string input)
+{
+    regex re("(.).\\1");
+    cmatch m;
+    return regex_search(input.c_str(), m, re);
+}
+
+bool isNiceStringPart1(string input)
 {
     if (has3Vowels(input) && hasNoBadDuels(input) && hasDoubleLetter(input))
     {
@@ -34,23 +48,41 @@ bool isNiceString(string input)
     return false;
 }
 
+bool isNiceStringPart2(string input)
+{
+    if (hasPairDouble(input) && hasBrokenDouble(input))
+    {
+        return true;
+    }
+    return false;
+}
+
 int main()
 {
-    cout << "testresult(0): " << isNiceString("cgjncqykgutfjhvb") << "\n";
-    cout << "testresult(1): " << isNiceString("josaajos") << "\n";
-    cout << "testresult(0): " << isNiceString("josaaaxy") << "\n";
-    cout << "testresult(0): " << isNiceString("josamaty") << "\n";
+    cout << "Part 1 testresult(0): " << isNiceStringPart1("cgjncqykgutfjhvb") << "\n";
+    cout << "Part 1 testresult(1): " << isNiceStringPart1("josaajos") << "\n";
+    cout << "Part 1 testresult(0): " << isNiceStringPart1("josaaaxy") << "\n";
+    cout << "Part 1 testresult(0): " << isNiceStringPart1("josamaty") << "\n";
+    cout << "Part 2 testresult(1): " << isNiceStringPart2("xyxy") << "\n";
+    cout << "Part 2 testresult(1): " << isNiceStringPart2("aabcdefgaaa") << "\n";
+    cout << "Part 2 testresult(0): " << isNiceStringPart2("aabcdefgaa") << "\n";
     string input = getInput(5);
     vector<string> lines;
     splitString(input, "\n", lines);
-    int nbNice = 0;
+    int nbNicePart1 = 0;
+    int nbNicePart2 = 0;
     for (int i = 0; i < lines.size(); i++)
     {
-        if (isNiceString(lines[i]))
+        if (isNiceStringPart1(lines[i]))
         {
-            nbNice++;
+            nbNicePart1++;
+        } 
+        if (isNiceStringPart2(lines[i]))
+        {
+            nbNicePart2++;
         }
     }
-    cout << "Part 1 Result < " << nbNice << " >\n";
+    cout << "Part 1 Result < " << nbNicePart1 << " >\n";
+    cout << "Part 2 Result < " << nbNicePart2 << " >\n";
     return 0;
 }
