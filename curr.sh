36d84ad5b16f6241b93e6d31b5b3a30ee93c1eaf
Francis Duvivier <francisduvivier@gmail.com>
2020-07-28 21:44:03 +0200

    🔧 Add shell script to compile and run code

---
+++ curr.sh
@@ -0,0 +1,4 @@
+CURR_DAY="day6"
+CURR_COMPILE_INPUT="$CURR_DAY/* util/*"
+g++ $CURR_COMPILE_INPUT -o bin/curr
+bin/curr
---
+++ day4.sh
@@ -0,0 +1,3 @@
+CURR_COMPILE_INPUT="day4/* util/* -lssl -lcrypto"
+g++ $CURR_COMPILE_INPUT -o bin/day4
+bin/day4
