f8ee335b23070f5c0d69baf154144d40be15b58e
Francis Duvivier <francisduvivier@gmail.com>
2020-07-29 20:11:28 +0200

    ✨ Start Day 9

---
+++ day9/day9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <regex>
+#include "../util/processing.h"
+using namespace std;
+const bool DEBUG_V = false;
+const bool DEBUG_I = false;
+
+
+int main()
+{
+    cout << "Day 9\n";
+    string input = getInput(9);
+    vector<string> lines;
+    splitString(input, "\n", lines);
+}
