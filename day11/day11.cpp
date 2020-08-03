1311d547c055b6cdebe0da3f517fc572909ab187
Francis Duvivier <francisduvivier@gmail.com>
2020-08-03 15:59:31 +0200

    ✨ Solve Day 11 Part 1

--- day11/day11.cpp
+++ day11/day11.cpp
@@ -1,9 +1,82 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include <iterator>
+#include <regex>
 using namespace std;
 const bool DEBUG_V = 0;
 const bool DEBUG_I = 0;
+
+string getNext(string currPass)
+{
+    bool finished = false;
+    for (int currIndex = currPass.length() - 1; !finished; currIndex--)
+    {
+        if (currIndex < 0)
+        {
+            cout << "End of options reached, password would wrap!\n";
+            throw "End of options reached, password would wrap!\n";
+        }
+        char newEnd = (currPass[currIndex]) + 1;
+        if (newEnd > 'z')
+        {
+            newEnd = 'a';
+        }
+        else
+        {
+            finished = true;
+        }
+        currPass[currIndex] = newEnd;
+    }
+    return currPass;
+}
+
+bool checkValid(string pass)
+{
+
+    if (regex_search(pass, regex("[iol]")))
+    {
+        return false;
+    }
+    bool foundTriple = false;
+    bool found2Double = false;
+    char currDouble = ' ';
+    int nbDouble = 0;
+    char prevChar = pass[0];
+    int nbSequence = 1;
+    for (string::iterator it = pass.begin() + 1; it != pass.end(); it++)
+    {
+        char currChar = *it;
+        if (prevChar + 1 == currChar)
+        {
+            nbSequence++;
+        }
+        else
+        {
+            nbSequence = 1;
+        }
+        if (nbSequence == 3)
+        {
+            foundTriple = true;
+        }
+
+        if (prevChar == currChar && prevChar != currDouble)
+        {
+            nbDouble++;
+            currDouble = currChar;
+        }
+        if (nbDouble == 2)
+        {
+            found2Double = true;
+        }
+        if (foundTriple && found2Double)
+        {
+            break;
+        }
+        prevChar = currChar;
+    }
+
+    return foundTriple && found2Double;
+}
 
 int main()
 {
@@ -11,12 +84,28 @@ int main()
     cout << "Day 11\n";
     string currState = input;
     bool solutionFound = false;
+    if (DEBUG_I)
+    {
+        cout << "getNext(\"abcdffaa\") (abcdffab) [" << getNext("abcdffaa") << "]\n";
+        cout << "getNext(\"abcdffaz\") (abcdffba) [" << getNext("abcdffaz") << "]\n";
+        cout << "getNext(\"abcdffzz\") (abcdfgaa) [" << getNext("abcdffzz") << "]\n";
+
+        cout << "checkValid(\"abcdffaa\") (1) [" << checkValid("abcdffaa") << "]\n";
+        cout << "checkValid(\"abcdflaa\") (0) [" << checkValid("abcdflaa") << "]\n";
+        cout << "checkValid(\"abcdfkaa\") (0) [" << checkValid("abcdfkaa") << "]\n";
+        cout << "checkValid(\"abkdffaa\") (0) [" << checkValid("abkdffaa") << "]\n";
+    }
     while (!solutionFound)
     {
         if (DEBUG_V)
         {
             cout << "currState [" << currState << "]\n";
         }
+        currState = getNext(currState);
+        if (checkValid(currState))
+        {
+            solutionFound = true;
+        }
     }
-    cout << "Part 1 Solution is < " << currState.length() << " >\n";
+    cout << "Part 1 Solution is < " << currState << " >\n";
 }
