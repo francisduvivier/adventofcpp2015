35a84b5bfad2119c3348d8288bbba3d18a762b6b
Francis Duvivier <francisduvivier@gmail.com>
2020-08-05 19:40:36 +0200

    ✨ Solve Day 16 Part 2

--- day16/day16.cpp
+++ day16/day16.cpp
@@ -14,15 +14,20 @@ int main()
     vector<string> lines;
     splitString(input, "\n", lines);
     regex tickerLineMatcher("children: [^3]|cats: [^7]|samoyeds: [^2]|pomeranians: [^3]|akitas: [^0]|vizslas: [^0]|goldfish: [^5]|trees: [^3]|cars: [^2]|perfumes: [^1]");
-    int matchingAunt = -1;
+    int matchingAuntPart1 = -1;
+    int matchingAuntPart2 = -1;
+    regex tickerLineMatcherPart2("children: [^3]|cats: [0-7](,|$)|samoyeds: [^2]|pomeranians: ([3-9]|[^0][0-9])|akitas: [^0]|vizslas: [^0]|goldfish: ([4-9]|[^0][0-9])|trees: [0-3](,|$)|cars: [^2]|perfumes: [^1]");
     for (int auntIndex = 0; auntIndex < lines.size(); auntIndex++)
     {
         if (!regex_search(lines[auntIndex].c_str(), tickerLineMatcher))
         {
-            matchingAunt = auntIndex;
-            break;
+            matchingAuntPart1 = auntIndex;
+        }
+        if (!regex_search(lines[auntIndex].c_str(), tickerLineMatcherPart2))
+        {
+            matchingAuntPart2 = auntIndex;
         }
     }
-
-    cout << "Part 1 solution is < " << lines[matchingAunt] << " >\n";
+    cout << "Part 1 solution is < " << lines[matchingAuntPart1] << " >\n";
+    cout << "Part 2 solution is < " << lines[matchingAuntPart2] << " >\n";
 }
