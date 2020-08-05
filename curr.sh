84352d543f33901b3cff806c5ab9639419ad60a0
Francis Duvivier <francisduvivier@gmail.com>
2020-08-05 18:23:44 +0200

    🔧 Add debug option to curr.sh

--- curr.sh
+++ curr.sh
@@ -1,5 +1,22 @@
 CURR_DAY="day15"
 CURR_COMPILE_INPUT="$CURR_DAY/*.cpp util/*"
+if [ "$DEBUG" == 1 ]
+then 
+g++ $CURR_COMPILE_INPUT -o "bin/curr" -g
+else
 g++ $CURR_COMPILE_INPUT -o "bin/curr"
+fi
+
+if [ $? != 0 ]
+then 
+echo "Compilation failed!"
+exit -1
+fi
+
 echo "Compilation [$CURR_DAY] Finished"
+if [ "$DEBUG" == 1 ]
+then 
+gdb "bin/curr"
+else
 "bin/curr"
+fi
