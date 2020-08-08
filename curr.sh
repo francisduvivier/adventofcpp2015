a1ef7045833ab0558ec4058983eadd9c9f158f39
Francis Duvivier <francisduvivier@gmail.com>
2020-08-08 12:59:52 +0200

    ✨ Start Day 22

--- curr.sh
+++ curr.sh
@@ -1,4 +1,4 @@
-CURR_DAY="day21"
+CURR_DAY="day22"
 CURR_COMPILE_INPUT="$CURR_DAY/*.cpp util/*"
 if [ "$DEBUG" == 1 ]
 then 
---
+++ day22/day22.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+const bool DEBUG_V = 0;
+const bool DEBUG_I = 0;
+using namespace std;
+void doPart1() {
+    cout << "--- Part 1 ---\n";
+
+}
+int main()
+{
+    cout << "--- Day 22: Wizard Simulator 20XX ---\n";
+    doPart1();
+}
