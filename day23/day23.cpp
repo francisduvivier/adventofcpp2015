06e88dd385c19c7246832af9205d508f5baddc60
Francis Duvivier <francisduvivier@gmail.com>
2020-08-10 14:00:40 +0200

    ✨ Solve Day 23 Part 2

--- day23/day23.cpp
+++ day23/day23.cpp
@@ -120,6 +120,20 @@ void doPart1(vector<Instruction *> &instructions, VM &vm) {
     }
     cout << "Part 1 solution is " << vm.registers['b' - 'a'] << "\n";
 }
+
+void doPart2(vector<Instruction *> &instructions, VM &vm) {
+    vm.registers[0] = 1;
+    while (vm.instructionPointer < instructions.size()) {
+        if (DEBUG_I) {
+            cout << "doing instruction " << instructions[vm.instructionPointer]->name << ", args[0] [" << instructions[vm.instructionPointer]->args[0] << "]\n";
+        }
+        instructions[vm.instructionPointer]->run();
+        if (DEBUG_I) {
+            cout << "curr pointer [" << vm.instructionPointer << "], regs a[" << vm.registers[0] << "] b[" << vm.registers[1] << "]\n";
+        }
+    }
+    cout << "Part 2 solution is " << vm.registers['b' - 'a'] << "\n";
+}
 void parseInstructions(vector<string> &lines, vector<Instruction *> &instructions, VM &vm) {
     {
         for (int i = 0; i < lines.size(); i++)
@@ -205,4 +219,8 @@ int main()
     vector<Instruction *> instructions;
     parseInstructions(lines, instructions, vm);
     doPart1(instructions, vm);
+    VM vm2 = VM();
+    instructions.clear();
+    parseInstructions(lines, instructions, vm2);
+    doPart2(instructions, vm2);
 }
