5a83558f63f38a1bc3509bef741e706a3e1e3188
Francis Duvivier <francisduvivier@gmail.com>
2020-08-10 09:40:30 +0200

    ✨ Start Day 23

---
+++ day23/day23.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <regex>
+#include <iostream>
+#include "../util/processing.h"
+const bool DEBUG_V = 0;
+const bool DEBUG_I = 0;
+using namespace std;
+
+void doPart1() {
+
+}
+int main()
+{
+    cout << "Day 23\n";
+    string input = getInput(23);
+    vector<string> lines;
+    splitString(input, "\n", lines);
+    doPart1();
+}
